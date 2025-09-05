f5bb9e61c2e371b2b95cdebe27dac2a8b99868cc
Zain <zainanees815@gmail.com>
2025-09-05 15:48:41 +0500

    lest co

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+   darkMode: 'class',
   content: ['./build/**/*.{html,php,css,js}'], // Scans all subfolders within 'build'
   theme: {
     extend: {},
